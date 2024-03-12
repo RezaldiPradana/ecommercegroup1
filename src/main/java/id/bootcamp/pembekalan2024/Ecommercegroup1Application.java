1e4b5316a28eabe81916350d0a6f081f02107e61
Fitria Salma <47936349+FitriaSalma@users.noreply.github.com>
2024-03-12 10:25:45 +0700

    first commit

---
+++ src/main/java/id/bootcamp/pembekalan2024/Ecommercegroup1Application.java
@@ -0,0 +1,13 @@
+package id.bootcamp.pembekalan2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ecommercegroup1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ecommercegroup1Application.class, args);
+	}
+
+}
---
+++ src/test/java/id/bootcamp/pembekalan2024/Ecommercegroup1ApplicationTests.java
@@ -0,0 +1,13 @@
+package id.bootcamp.pembekalan2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ecommercegroup1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
