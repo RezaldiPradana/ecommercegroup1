4e0df5546d6c4712c7dab6644c55cc364f1bb8af
Asyifa-Zahwa <azahwa201@gmail.com>
2024-03-12 16:22:15 +0700

    update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,7 @@
+INSERT INTO m_user (username, password, role, created_by, created_on)
+VALUES
+	('admin', 'admin', 'admin', 1, NOW()),
+	('customer', 'customer', 'customer', 1, NOW());
 INSERT INTO stock (item_code, product_name, product_type, quantity, price, created_by, created_on)
 VALUES
     ('CLN001', 'Celana Panjang Jeans', 'Celana', 20, 150000, 1, NOW()),
@@ -11,3 +15,11 @@ VALUES
     ('BJU004', 'Baju Koko Pria Dewasa', 'Baju', 26, 190000, 1, NOW()),
     ('CLN004', 'Celana Training Olahraga', 'Celana', 19, 140000, 1, NOW()),
     ('BJU005', 'Baju Kaos Polos Anak', 'Baju', 24, 170000, 1, NOW());
+ insert into cart (id_user, created_by, created_on)
+ values
+ 	(1, 1, NOW());
+ INSERT INTO cart_detail (id_cart, item_code, quantity, price, created_by, created_on)
+ VALUES
+ 	(1, 'CLN001', 2, '30000', 1, NOW()),
+ 	(1, 'TOP001', 3, '240000', 1, NOW());
+ 
