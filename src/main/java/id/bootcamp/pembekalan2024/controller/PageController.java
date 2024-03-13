27aa197cd365899570a1d87561015ff06953a083
xsis.novegaa <xsis.novegaa@xl.co.id>
2024-03-14 00:21:19 +0700

    login bisa cuma belum sama spring security

--- src/main/java/id/bootcamp/pembekalan2024/controller/PageController.java
+++ src/main/java/id/bootcamp/pembekalan2024/controller/PageController.java
@@ -5,30 +5,57 @@ import id.bootcamp.pembekalan2024.model.DataUser;
 import id.bootcamp.pembekalan2024.repositories.UserRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class PageController {
+    @Autowired
     UserRepository userRepository;
+
     @GetMapping("/")
     public String loginForm(HttpServletRequest servlet,Model model) {
         String title_web = "Login";
         model.addAttribute("user", new DataUser());
-        return "index.html";
+        return "index";
     }
     @PostMapping("/")
     public String login(@ModelAttribute("user") UserEntity dataUser, HttpSession session) {
-        UserEntity existingUser = userRepository.getUserByUSernameAndPassword(dataUser.getUsername(),dataUser.getPassword());
+        DataUser existingUser = userRepository.getUserByUSernameAndPassword(dataUser.getUsername(),dataUser.getPassword());
         if (existingUser != null && existingUser.getPassword().equals(dataUser.getPassword())) {
             session.setAttribute("loggedInUser", existingUser);
             if ("admin".equals(existingUser.getRole())) {
-                return "redirect:/list";
+                return "redirect:/stock";
             } else {
-
+                return "redirect:/kasir";
             }
         }
         return "redirect:/";
+    }
+
+    @GetMapping("/stock")
+    public String stock (HttpServletRequest servlet) {
+        String title_web = "stock";
+        return "stock";
+    }
+
+    @GetMapping("/register")
+    public String register (HttpServletRequest servlet) {
+        String title_web = "register";
+        return "register";
+    }
+
+    @GetMapping("/cart_detail")
+    public String cart_detail (HttpServletRequest servlet) {
+        String title_web = "cart_detail";
+        return "cart_detail";
+    }
+
+    @GetMapping("/cart")
+    public String cart (HttpServletRequest servlet) {
+        String title_web = "cart";
+        return "cart";
     }
 }
--- src/main/java/id/bootcamp/pembekalan2024/repositories/UserRepository.java
+++ src/main/java/id/bootcamp/pembekalan2024/repositories/UserRepository.java
@@ -1,17 +1,17 @@
 package id.bootcamp.pembekalan2024.repositories;
 
 import id.bootcamp.pembekalan2024.entities.UserEntity;
+import id.bootcamp.pembekalan2024.model.DataUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<UserEntity, Long> {
+public interface UserRepository extends JpaRepository<DataUser, Long> {
     @Query(nativeQuery = true,
-            value = "select * from user_data "
-                    + "where username = :username and password = :password")
-    public UserEntity getUserByUSernameAndPassword(
+            value = "select * from user_data where username = :username and password = :password")
+    public DataUser getUserByUSernameAndPassword(
             @Param("username") String username,
             @Param("password") String password
     );
