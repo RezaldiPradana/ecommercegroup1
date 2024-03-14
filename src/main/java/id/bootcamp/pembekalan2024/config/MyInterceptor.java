640e53b9ca326e2282162fe9f5544ae95146f345
RezaldiPradana <cihuyaldi@gmail.com>
2024-03-14 14:23:52 +0700

    first

---
+++ src/main/java/id/bootcamp/pembekalan2024/config/MyInterceptor.java
@@ -0,0 +1,28 @@
+package id.bootcamp.pembekalan2024.config;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+public class MyInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+            throws Exception {
+        // Method ini dipanggil sebelum penanganan permintaan aktual (controller method)
+        return true; // Return true jika permintaan harus dilanjutkan, false jika tidak
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+                           ModelAndView modelAndView) throws Exception {
+        // Method ini dipanggil setelah penanganan permintaan aktual tetapi sebelum menampilkan tampilan
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+            throws Exception {
+        // Method ini dipanggil setelah penanganan permintaan selesai, termasuk menampilkan tampilan
+    }
+}
---
+++ src/main/java/id/bootcamp/pembekalan2024/model/DataNewUser.java
@@ -0,0 +1,4 @@
+package id.bootcamp.pembekalan2024.model;
+
+public class DataNewUser {
+}
